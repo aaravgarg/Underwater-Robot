7ffef3fff5a5871758be06cb91fba973a4a445c8
asong18 <145512500+asong18@users.noreply.github.com>
2024-10-02 12:36:56 -0400

    Update CombinedCode.ino

--- CombinedCode/CombinedCode.ino
+++ CombinedCode/CombinedCode.ino
@@ -1,91 +1,131 @@
 #include <Stepper.h>
 #include <Servo.h>
+
 Servo rightServo;  // Create a servo object to control the right servo motor
 Servo leftServo;   // Create a servo object to control the left servo motor
+Servo massServo;
 
+// Change this to the number of steps on your motor
 #define STEPS 100
+
 const int topOpenPin = 27;
 const int topClosePin = 45;
 const int bottomOpenPin = 44;
-const int bottomClosePin = 26;
+const int bottomClosePin = 26; 
 
-
-bool topOpen = false;
-bool topClose = false;
-bool bottomOpen = false;
-bool bottomClose = false;
-
-// create an instance of the topStepper class, specifying the number of steps of the motor and the pins it's attached to
-Stepper topStepper(STEPS, 22, 23, 24, 25);
+// Create instances of the Stepper class for top and bottom steppers
+Stepper syringeStepper(STEPS, 36, 37, 38, 39);
 
 enum Direction { FORWARD, BACKWARD };
-Direction topDirection = FORWARD;
-bool topMoving = false;
+Direction syringeDirection = FORWARD;
+bool syringeMoving = false;
 
 void setup() {
   Serial.begin(9600);
-  // set the speed of the motor to 30 RPMs
-  topStepper.setSpeed(50);
+  
+  // Set the speed of the motors to 50 RPMs
+  syringeStepper.setSpeed(50);
+  
   pinMode(topOpenPin, INPUT_PULLUP);
   pinMode(topClosePin, INPUT_PULLUP);
-  rightServo.attach(10);   // Attach the right servo motor to pin 10
-  leftServo.attach(11);    // Attach the left servo motor to pin 11
-}
+  pinMode(bottomOpenPin, INPUT_PULLUP);
+  pinMode(bottomClosePin, INPUT_PULLUP);
+  
+  rightServo.attach(43);   // Attach the right servo motor to pin 43
+  leftServo.attach(49);    // Attach the left servo motor to pin 49
+  massServo.attach(42);
 
+  Serial.println("Ready for commands:");
+  Serial.println("o - Open wings");
+  Serial.println("c - Close wings");
+  Serial.println("s - Move syringe steppers");
+  Serial.println("m - Move moving mass");
+  Serial.println("r - Rotate moving mass");
+  
+  rightServo.write(25);
+  leftServo.write(120);
+}
 
 void loop() {
-  if (Serial.available() > 0) { // Check if data is available
-    char command = Serial.read(); // Read the incoming character
-    // Call functions based on the input
-    if (command == 'O') {
-      openWing();  
-    } else if (command == 'C') {
-      closeWing(); 
-    } else if (command == 'S'){
-      moveTopStepper();
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    switch (command) {
+      case 'o':
+        openWing();
+        break;
+      case 'c':
+        closeWing();
+        break;
+      case 's':
+        moveSyringeSteppers();
+        break;
+      /*
+      case 'm':
+        moveMovingMass();
+        break;
+        */
+      case 'r':
+        rotateMovingMass();
+        break;
+      default:
+        Serial.println("Invalid command");
     }
   }
 }
 
-void openWing(){
-  // Open the wings
+void openWing() {
+  Serial.println("Opening wings");
   for (int pos = 25, poss = 120; pos <= 95 && poss >= 50; pos += 1, poss -= 1) {
-    rightServo.write(pos);   // Move the right servo
-    leftServo.write(poss);   // Move the left servo
-    delay(15);               // Wait for the servos to reach the position
+    rightServo.write(pos);
+    leftServo.write(poss);
+    delay(15);
   }
+  Serial.println("Wings opened");
 }
 
-void closeWing(){
-  // Close the wings
+void closeWing() {
+  Serial.println("Closing wings");
   for (int pos = 95, poss = 50; pos >= 25 && poss <= 120; pos -= 1, poss += 1) {
-    rightServo.write(pos);   // Move the right servo
-    leftServo.write(poss);   // Move the left servo
-    delay(15);               // Wait for the servos to reach the position
+    rightServo.write(pos);
+    leftServo.write(poss);
+    delay(15);
   }
+  Serial.println("Wings closed");
 }
 
-void moveTopStepper(){
-  if (topMoving) {
-    if (topDirection == FORWARD) {
-      topStepper.step(STEPS);
+void moveSyringeSteppers() {
+  Serial.println("Moving syringe steppers");
+  
+  while (true) { // Continuous loop
+    if (syringeDirection == FORWARD) {
+      syringeStepper.step(1); // Move one step forward
       if (digitalRead(topClosePin) == LOW || digitalRead(bottomClosePin) == LOW) {
-        topDirection = BACKWARD;
-        topMoving = false;  // Stop moving until direction change is completed
+        syringeDirection = BACKWARD; // Change direction
+        Serial.println("Reached close position, changing direction");
+        break; // Exit loop
       }
-    } else if (topDirection == BACKWARD) {
-      topStepper.step(-STEPS);
+    } else {
+      syringeStepper.step(-1); // Move one step backward
       if (digitalRead(topOpenPin) == LOW || digitalRead(bottomOpenPin) == LOW) {
-        topDirection = FORWARD;
-        topMoving = false;  // Stop moving until direction change is completed
+        syringeDirection = FORWARD; // Change direction
+        Serial.println("Reached open position, changing direction");
+        break; // Exit loop
       }
     }
-  } else {
-    // Start moving in the current direction if not already moving
-    topMoving = true;
+    delay(10); // Small delay to control speed and avoid overwhelming the serial output
   }
+  
 }
 
-
-
-
+// Uncomment and implement these functions if needed in future testing
+/*
+void moveMovingMass() {
+  Serial.println("Moving the moving mass");
+}
+*/
+void rotateMovingMass() {
+  for (int pos = 95; pos >= 25; pos -= 1) {
+    massServo.write(pos);
+    delay(15);
+  }
+}
